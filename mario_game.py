668e0adbdc3a51941baf6f9ae8882e5005b32a5d
pyredouane <113345818+ItIsRd1@users.noreply.github.com>
2022-10-12 16:00:10 +0100

    Add files via upload

---
+++ mario_game.py
@@ -0,0 +1,168 @@
+# 2d game
+
+import pygame
+
+speed_jumping = 10
+height_jump = 0.30
+frames = 60
+
+# check errors
+check_error = pygame.init()
+
+if check_error[1] >= 1:
+    print(f"Error {check_error[1]}")
+else:
+    print("No Errors")
+
+# window settings
+
+frame_size_x = 800
+frame_size_y = 600
+
+my_title = "Mario Game"
+
+my_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
+
+pygame.display.set_caption(my_title)
+
+bg = pygame.image.load(r"C:\Users\user\Desktop\VSCODE\photos\back.jpg")
+
+# hero movment
+
+left = False
+right = False
+moves = 0
+
+move_left = [pygame.image.load(r"C:\Users\user\Desktop\VSCODE\photos\mario_NoMove_l.png"),
+             pygame.image.load(
+                 r"C:\Users\user\Desktop\VSCODE\photos\mario_move2_l.png"),
+             pygame.image.load(
+                 r"C:\Users\user\Desktop\VSCODE\photos\mario_move2_l.png"),
+             pygame.image.load(
+                 r"C:\Users\user\Desktop\VSCODE\photos\mario_move1_l.png")]
+
+move_right = [pygame.image.load(r"C:\Users\user\Desktop\VSCODE\photos\mario_NoMove_r.png"),
+              pygame.image.load(
+                  r"C:\Users\user\Desktop\VSCODE\photos\mario_move2_r.png"),
+              pygame.image.load(
+                  r"C:\Users\user\Desktop\VSCODE\photos\mario_move2_r.png"),
+              pygame.image.load(
+                  r"C:\Users\user\Desktop\VSCODE\photos\mario_move1_r.png")]
+
+hero_r = pygame.image.load(
+    r"C:\Users\user\Desktop\VSCODE\photos\mario_NoMove_r.png")
+hero_l = pygame.image.load(
+    r"C:\Users\user\Desktop\VSCODE\photos\mario_NoMove_l.png")
+
+# buttons
+start = pygame.image.load(r"C:\Users\user\Desktop\VSCODE\photos\start_img.png")
+exit = pygame.image.load(r"C:\Users\user\Desktop\VSCODE\photos\exit_img.png")
+
+# square
+
+object_size = 40
+object_x = 20
+object_y = 450
+x_spase = 0
+y_spase = 0
+
+# colors
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# texts
+
+start_font = pygame.font.SysFont(None, 20, True, True)
+start_text = start_font.render(
+    "press '1' button to lanch the game", True, WHITE)
+exit_font = pygame.font.SysFont(None, 20, True, True)
+exit_text = exit_font.render(
+    "press 'ECS' button to exit the game", True, WHITE)
+
+# game lunching
+
+game_over = False
+is_jumping = False
+is_start = False
+stop_left = False
+stop_right = False
+stop_start_game = 0
+is_start = False
+
+while not game_over:
+    pygame.time.Clock().tick(frames)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_1]:
+        is_start = True
+    if key[pygame.K_ESCAPE]:
+        game_over = True
+
+    if (key[pygame.K_LEFT] or key[pygame.K_q]) and object_x > 0:
+        object_x -= 3
+        left = True
+        right = False
+        stop_left = True
+        stop_right = False
+    elif (key[pygame.K_RIGHT] or key[pygame.K_d]) and object_x + object_size < frame_size_x:
+        object_x += 3
+        right = True
+        left = False
+        stop_left = False
+        stop_right = True
+    else:
+        right = False
+        left = False
+        moves = 0
+
+    if not is_jumping:
+        if key[pygame.K_SPACE]:
+            is_jumping = True
+    else:
+        if speed_jumping >= -10:
+            neg = 1
+            if speed_jumping < 0:
+                neg = -1
+            object_y -= (speed_jumping ** 2) * height_jump * neg
+            speed_jumping -= 1
+        else:
+            speed_jumping = 10
+            is_jumping = False
+
+    my_screen.blit(bg, (0, 0))
+    # my_screen.fill(BLACK)
+    if not is_start:
+        my_screen.blit(start_text, (1, 560))
+    if not is_start:
+        my_screen.blit(exit_text, (1, 580))
+
+    if not is_start:
+        my_screen.blit(start, (250, 100))
+        my_screen.blit(exit, (267, 300))
+    else:
+        if left:
+            my_screen.blit(move_left[moves // 5], (object_x, object_y))
+            moves += 1
+            if moves == 20:
+                moves = 0
+        elif right:
+            my_screen.blit(move_right[moves // 5], (object_x, object_y))
+            moves += 1
+            if moves == 20:
+                moves = 0
+        else:
+            if not stop_right:
+                my_screen.blit(hero_l, (object_x, object_y))
+            elif not stop_left:
+                my_screen.blit(hero_r, (object_x, object_y))
+
+    pygame.display.update()
